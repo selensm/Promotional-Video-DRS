962a2cba358b940e7ab9d7d7495e8c0af1523821
selensm <selensmehmed@gmail.com>
2020-09-18 16:38:27 +0200

    Hotspots engine + placeholder hotspots

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,194 @@ let options = {
 }
 
 videojs('video', options);
+
+
+
+// Source: 
+
+const fps = 30;     // ## adjust this to set the frames per second precision on the hotspot appearance (lower = less cpu used)
+const debug = true; // ## set to true to get console.log output, use   video.log('text')
+// ## SETTINGS END
+
+const msInterval = Math.floor(1000 / fps); // calculate how many ms per loop to match desired FPS. Rounded down
+let engine; // declare a variable that will be used for the interval loop
+
+let video = {
+    log: (message = 'Missing log text') => {
+        if (debug) {
+            console.log(message);
+        }
+    },
+    hotspots: {
+        running: false,
+        init: () => {
+            video.log('video hotspot engine: init');
+            const elmsVideo = document.querySelectorAll('video');   // grab all videos on the page
+            elmsVideo.forEach((elmVideo) => {                       // loop through the parents of the video elements
+                elmVideo.parentElement.classList.add('videoHotspotsParent');
+                elmVideo.addEventListener('play', (event) => {      // add eventlistener play on videos
+                    if (!video.hotspots.running) {                   // start engine, if it is not running already
+                        video.hotspots.on();
+                    }
+                });
+                elmVideo.addEventListener('seeked', (event) => {      // add eventlistener play on videos
+                    if (!video.hotspots.running) {                   // start engine, if it is not running already
+                        video.hotspots.on(true);
+                    }
+                });
+                elmVideo.addEventListener('pause', (event) => {         // add eventlistener stop/pause on videos
+                    if (video.hotspots.running) {        // if engine is running
+                        let videoPlaying = false;       // check if all videos are stopped/paused
+                        elmsVideo.forEach((elmVideo) => {
+                            if (!elmVideo.paused) {
+                                videoPlaying = true;
+                            }
+                        });
+                        if (!videoPlaying) {
+                            video.hotspots.off();   // if all videos are NOT playing we can turn off the loop engine
+                        }
+                    }
+                });
+            });
+        },
+        on: (isSeeked = false) => {
+            // start the interval loop
+            video.log('video hotspot engine: on');
+            if (!video.hotspots.running) {       // only start it if it isn't already running
+                video.hotspots.running = true;  // make sure to tell our boolean that we are turning on the engine
+            }
+            engine = setInterval(() => {        // start the interval engine
+                video.log('engine loop');
+                video.hotspots.update(isSeeked);
+            }, msInterval);
+        },
+        off: () => {
+            // kill the interval var
+            video.log('video hotspot engine: off');
+            video.hotspots.running = false;     // make sure to tell our boolean that the engine is being stopped
+            clearInterval(engine);              // stop the engine
+        },
+        update: () => {
+            hotspots.forEach((hotspot, index) => {
+                if (hotspot.active) {
+                    // get video element for hotspot
+                    const video = document.querySelector(`#${hotspot.videoId}>video`);
+                    
+                    if (video) {
+                        const now = video.currentTime;
+                        const elmHotspotCheck = document.querySelector(`#hotspotId${index}`);
+                        
+
+                        if (hotspot.markIn > now || hotspot.markOut <= now) {
+                            // check to see if element with the current hotspot id exists
+                            if (elmHotspotCheck) {
+                                // remove hotspot element
+                                const elmHotspot = document.querySelector(`#hotspotId${index}`);
+                                elmHotspot.parentElement.removeChild(elmHotspot);
+                                hotspot.onscreen = false; // clear on-screen flag for the current hotspot
+                            }
+                        } else if (hotspot.markIn <= now && hotspot.markOut > now) {
+                            if (!elmHotspotCheck) { // only draw new hotspot if it isn't already drawn
+                                let elmHotspot = document.createElement('a');
+                                elmHotspot.id = `hotspotId${index}`;
+                                elmHotspot.className = 'hotspot';
+                                if (hotspot.ui.title) {
+                                    elmHotspot.title = hotspot.ui.title;
+                                }
+                                if (hotspot.ui.text && hotspot.ui.text != "") {
+                                    elmHotspot.innerHTML = hotspot.ui.text;
+                                }
+                                let css = "";
+                                css += `width: ${hotspot.sizeX}%;`;
+                                css += `height: ${hotspot.sizeY}%;`;
+                                css += `left: ${hotspot.posX}%;`;
+                                css += `top: ${hotspot.posY}%;`;
+                                css += `${hotspot.ui.style};`;
+                                if (hotspot.ui.type == 'image') {
+                                    // insert image css
+                                    css += `background-image: url(${hotspot.ui.image});`;
+                                    elmHotspot.classList.add('image');
+                                }
+                                elmHotspot.style = css;
+                                if (hotspot.hotspot.type == 'link') {
+                                    // it's a link, set target and href
+                                    elmHotspot.href = hotspot.hotspot.url;
+                                    elmHotspot.target = hotspot.hotspot.target;
+                                } else {
+                                    // it's a function, set an eventlistener for click
+                                    elmHotspot.addEventListener('click', (event) => {
+                                        event.preventDefault(); // prevent the normal action when clicking on an <a> tag
+                                        hotspot.hotspot.func(); // run the function that the hotspot JSON contains for this hotspot
+                                    });
+                                }
+                                video.parentElement.appendChild(elmHotspot);
+                                video.pause();
+                            }
+                        }
+                    }
+                }
+            });   
+        },
+        remove: () => {
+            // kill all hotspot related functions, json feed and DOM elements - use  video.hotspots.remove()  to do this
+            video.log('video hotspot engine: cleanup');
+            video.hotspots.off();                                               // turn off engine
+            const elmsHotspots = document.querySelectorAll('a.hotspot');        // find all hotspot DOM elements
+            elmsHotspots.forEach((elmHotspot) => {                              // loop through hotspots
+                elmHotspot.parentElement.removeChild(elmHotspot);               // remove current hotspot
+            });
+            delete video;                                                       // remove the variable from memory
+            delete hotspots;                                                    // remove the variable from memory
+        }
+
+    }
+}
+// end
+
+
+const hotspots = [
+    {
+        active: true,
+        videoId: "video",
+        markIn: 20.4,
+        markOut: 21,
+        sizeX: 30,
+        sizeY: 32,
+        posX: 52,
+        posY: 6,
+        ui: {
+            type: "box",
+            image: "",
+            style: "border: none; background-color: var(--blue); oppacity: 50%; padding: .3rem;"
+        },
+        hotspot: {
+            type: "link",
+            url: "",
+            target: "_blank"
+        }
+    },
+    {
+        active: true,
+        videoId: "video",
+        markIn: 25,
+        markOut: 27,
+        sizeX: 30,
+        sizeY: 32,
+        posX: 52,
+        posY: 6,
+        ui: {
+            type: "box",
+            image: "",
+            style: `border: none; background-color: #fff; color: #000;`,
+            text: "jikjkhukhkuhk"
+        },
+        hotspot: {
+            type: "link",
+            url: "",
+            target: "_blank"
+        }
+    }
+];
+
+video.hotspots.init();
+
